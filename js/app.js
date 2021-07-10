0b8ba10261cc05fb3ac9cdfe30b59ff3d62f8daf
Lucas Matheus Gomes da Silva <lucasmgsilva@hotmail.com>
2021-07-09 22:47:46 -0300

    Commit Inicial

---
+++ js/app.js
@@ -0,0 +1,104 @@
+const app = {
+  data() {
+    return {
+      connection: {
+        host: 'broker.emqx.io',
+        port: 8083,
+        endpoint: '/mqtt',
+        clean: true, // Reserved session
+        connectTimeout: 4000, // Time out
+        reconnectPeriod: 4000, // Reconnection interval
+        // Certification Information
+        clientId: 'mqttjs_3be2c321',
+        username: 'emqx_test',
+        password: 'emqx_test',
+      },
+      subscription: {
+        topic: '0f101c78-e114-11eb-ba80-0242ac130004/casa/sala/sensor/temperatura/leitura/',
+        qos: 0,
+      },
+      // publish: {
+      //   topic: 'topic/browser',
+      //   qos: 0,
+      //   payload: '{ "msg": "Hello, I am browser." }',
+      // },
+      receiveNews: [],
+      qosList: [
+        { label: 0, value: 0 },
+        { label: 1, value: 1 },
+        { label: 2, value: 2 },
+      ],
+      client: {
+        connected: false,
+      },
+      subscribeSuccess: false,
+    }
+  },
+  methods: {
+    // Create connection
+    createConnection() {
+      // Connect string, and specify the connection method used through protocol
+      // ws unencrypted WebSocket connection
+      // wss encrypted WebSocket connection
+      // mqtt unencrypted TCP connection
+      // mqtts encrypted TCP connection
+      // wxs WeChat mini app connection
+      // alis Alipay mini app connection
+      const { host, port, endpoint, ...options } = this.connection
+      const connectUrl = `ws://${host}:${port}${endpoint}`
+      try {
+        this.client = mqtt.connect(connectUrl, options)
+      } catch (error) {
+        console.log('mqtt.connect error', error)
+      }
+      this.client.on('connect', () => {
+        console.log('Connection succeeded!')
+      })
+      this.client.on('error', error => {
+        console.log('Connection failed', error)
+      })
+      this.client.on('message', (topic, message) => {
+        this.receiveNews.push(message.toString())
+        console.log(`Received message ${message} from topic ${topic}`)
+      })
+    },
+    doSubscribe() {
+      const { topic, qos } = this.subscription
+      this.client.subscribe(topic, { qos }, (error, res) => {
+        if (error) {
+          console.log('Subscribe to topics error', error)
+          return
+        }
+        this.subscribeSuccess = true
+        console.log('Subscribe to topics res', res)
+        })
+    },
+    doUnSubscribe() {
+      const { topic } = this.subscription
+      this.client.unsubscribe(topic, error => {
+        if (error) {
+          console.log('Unsubscribe error', error)
+        }
+      })
+    },
+    destroyConnection() {
+      if (this.client.connected) {
+        try {
+          this.client.end()
+          this.client = {
+            connected: false,
+          }
+          console.log('Successfully disconnected!')
+        } catch (error) {
+          console.log('Disconnect failed', error.toString())
+        }
+      }
+    }
+  },
+  mounted() {
+      this.createConnection(),
+      this.doSubscribe()
+  },
+}
+
+Vue.createApp(app).mount('#app')
