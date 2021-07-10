312535c7ff60b2c4cbaa1131bd8a9dcdbaab7f42
Lucas Matheus Gomes da Silva <lucasmgsilva@hotmail.com>
2021-07-10 12:48:47 -0300

    Desenvolvimento do cabeçalho, sessão de conteúdo e rodapé

--- js/app.js
+++ js/app.js
@@ -14,7 +14,10 @@ const app = {
         password: 'emqx_test',
       },
       subscription: {
-        topic: '0f101c78-e114-11eb-ba80-0242ac130004/casa/sala/sensor/temperatura/leitura/',
+        topic: [
+          '0f101c78-e114-11eb-ba80-0242ac130004/casa/sala/sensor/temperatura/leitura/',
+          '0f101c78-e114-11eb-ba80-0242ac130004/casa/sala/sensor/umidade/leitura/'
+        ],
         qos: 0,
       },
       // publish: {
@@ -22,7 +25,8 @@ const app = {
       //   qos: 0,
       //   payload: '{ "msg": "Hello, I am browser." }',
       // },
-      receiveNews: [],
+      dht_temperature: ['Carregando...'],
+      dht_humidity: ['Carregando...'],
       qosList: [
         { label: 0, value: 0 },
         { label: 1, value: 1 },
@@ -58,13 +62,19 @@ const app = {
         console.log('Connection failed', error)
       })
       this.client.on('message', (topic, message) => {
-        this.receiveNews.push(message.toString())
+
+        if (topic == this.subscription.topic[0]){ //Sensor temperatura
+          this.dht_temperature.push(message.toString())
+        } else if (topic == this.subscription.topic[1]){ //Sensor umidade
+          this.dht_humidity.push(message.toString())
+        }
+
         console.log(`Received message ${message} from topic ${topic}`)
       })
     },
     doSubscribe() {
       const { topic, qos } = this.subscription
-      this.client.subscribe(topic, { qos }, (error, res) => {
+       this.client.subscribe(topic[0], { qos }, (error, res) => {
         if (error) {
           console.log('Subscribe to topics error', error)
           return
@@ -72,6 +82,15 @@ const app = {
         this.subscribeSuccess = true
         console.log('Subscribe to topics res', res)
         })
+
+        this.client.subscribe(topic[1], { qos }, (error, res) => {
+          if (error) {
+            console.log('Subscribe to topics error', error)
+            return
+          }
+          this.subscribeSuccess = true
+          console.log('Subscribe to topics res', res)
+          })
     },
     doUnSubscribe() {
       const { topic } = this.subscription
